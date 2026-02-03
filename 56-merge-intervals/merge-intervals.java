3facf1be3e3cbfe378f156863fed3db1d98a1f01
Aayushi Sinha <89191076+aashisinha8@users.noreply.github.com>
2026-02-03 13:28:00 +0530

    Time: 7 ms (98.62%) | Memory: 49.4 MB (20.78%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        int idx = 0; // index of last merged interval
+
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[idx][1] >= intervals[i][0]) {
+                // overlap → merge
+                intervals[idx][1] =
+                    Math.max(intervals[idx][1], intervals[i][1]);
+            } else {
+                // no overlap → move forward
+                idx++;
+                intervals[idx] = intervals[i];
+            }
+        }
+
+        return Arrays.copyOf(intervals, idx + 1);
+    }
+}
