1b5cc1ef6b59a51ecf7a6c5d726b6a8f10d4b4ad
Aayushi Sinha <89191076+aashisinha8@users.noreply.github.com>
2026-05-07 14:16:33 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (71.97%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
@@ -1,14 +1,17 @@
 class Solution {
     public int kthGrammar(int n, int k) {
+        // Base row
         if(n==1){
             return 0;
         }
         //consider it as a tree  for like every 0 two chile 01 and 1 as paret with two chils 1 and 0
+         // Find parent in previous row
         int parent=kthGrammar(n-1,(k+1)/2);
+        // Odd index -> same as parent
         if(k%2==1){
             return parent;
         }
-        
+        // Even index -> flipped value
             return 1-parent;
         
     }
