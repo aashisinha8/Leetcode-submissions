999d41dbf37961f6d880df683cf51e3d34c18bec
Aayushi Sinha <89191076+aashisinha8@users.noreply.github.com>
2026-06-16 14:02:24 +0530

    Time: 7 ms (97.90%) | Memory: 65.3 MB (38.45%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -3,17 +3,14 @@ class Solution {
     public List<List<String>> partition(String s) {
 
         List<List<String>> ans = new ArrayList<>();
-        List<String> curr=new ArrayList<>();
+        List<String> curr = new ArrayList<>();
 
-        backtrack(0, s,curr , ans);
+        backtrack(0, s,curr, ans);
 
         return ans;
     }
 
-    private void backtrack(int start,
-                           String s,
-                           List<String> curr,
-                           List<List<String>> ans) {
+    private void backtrack(int start,String s,List<String> curr,List<List<String>> ans) {
 
         if(start == s.length()) {
 
@@ -21,31 +18,20 @@ class Solution {
             return;
         }
 
-        for(int end = start;
-            end < s.length();
-            end++) {
+        for(int end = start;end < s.length();end++) {
 
             if(isPalindrome(s, start, end)) {
 
-                curr.add(
-                    s.substring(start, end + 1)
-                );
+                curr.add(s.substring(start, end + 1));
 
-                backtrack(
-                    end + 1,
-                    s,
-                    curr,
-                    ans
-                );
+                backtrack(end + 1,s,curr,ans);
 
                 curr.remove(curr.size() - 1);
             }
         }
     }
 
-    private boolean isPalindrome(String s,
-                                 int left,
-                                 int right) {
+    private boolean isPalindrome(String s, int left, int right) {
 
         while(left < right) {
 
