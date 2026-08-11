1558f1dadccbb89bfe34b687e80df18bab5feaf3
Aayushi Sinha <89191076+aashisinha8@users.noreply.github.com>
2026-08-11 13:03:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (40.54%) - LeetSync

---
+++ 420-strong-password-checker/strong-password-checker.java
@@ -0,0 +1,66 @@
+class Solution {
+    public int strongPasswordChecker(String password) {
+        int n = password.length();
+
+        boolean lower = false, upper = false, digit = false;
+
+        for (char c : password.toCharArray()) {
+            if (Character.isLowerCase(c)) lower = true;
+            else if (Character.isUpperCase(c)) upper = true;
+            else if (Character.isDigit(c)) digit = true;
+        }
+
+        int missing = 0;
+        if (!lower) missing++;
+        if (!upper) missing++;
+        if (!digit) missing++;
+
+        if (n < 6)
+            return Math.max(missing, 6 - n);
+
+        int replace = 0;
+
+        int one = 0;
+        int two = 0;
+
+        for (int i = 0; i < n;) {
+
+            int j = i;
+
+            while (j < n && password.charAt(j) == password.charAt(i))
+                j++;
+
+            int len = j - i;
+
+            if (len >= 3) {
+                replace += len / 3;
+
+                if (len % 3 == 0)
+                    one++;
+                else if (len % 3 == 1)
+                    two++;
+            }
+
+            i = j;
+        }
+
+        if (n <= 20)
+            return Math.max(missing, replace);
+
+        int delete = n - 20;
+
+        int remain = delete;
+
+        int use = Math.min(remain, one);
+        replace -= use;
+        remain -= use;
+
+        use = Math.min(remain / 2, two);
+        replace -= use;
+        remain -= use * 2;
+
+        replace -= remain / 3;
+
+        return delete + Math.max(missing, replace);
+    }
+}
